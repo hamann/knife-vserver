ace8199368bc294fc3674525a905129b8d24e0d5
Holger Amann <holger@sauspiel.de>
2013-11-20 18:25:10 +0100

    add flags, memory limits and tinc interfaces

--- lib/knife-vserver/host.rb
+++ lib/knife-vserver/host.rb
@@ -17,7 +17,7 @@ module Knife
       end
 
       def container_exist?(name)
-        @containers.select { |n| n.name == name }.count > 0
+        false #@containers.select { |n| n.name == name }.count > 0
       end
 
       def add_new_container(container)
@@ -32,6 +32,8 @@ module Knife
 
         cmd_addresses = Array.new
         container.interfaces.each do |iface|
+          next if iface.is_tinc_interface
+
           iface.device = dummy_ifs.slice!(0)
           cmd_addresses << "--interface #{iface.device}:#{iface.address}/#{iface.netmask}"
         end
@@ -39,7 +41,7 @@ module Knife
         create_cmd_args = "--hostname #{container.hostname} --context #{container.ctx} " +
           "#{cmd_addresses.join(" ")} -- -d #{container.distribution}"
 
-        template_path = "/etc/vservers/templates/#{container.distribution}-base.tar.gz"
+        template_path = "#{@config_path}/templates/#{container.distribution}-base.tar.gz"
         if ShellCommand.exec("test -e #{template_path}",
                              session,
                              { :dont_raise_error => true }).succeeded?
@@ -50,13 +52,107 @@ module Knife
         end
 
         puts "Executing #{create_cmd}"
-        ShellCommand.exec(create_cmd, session)
+        #ShellCommand.exec(create_cmd, session)
+
+        add_flags(container)
+        apply_memory_limits(container)
+        mark_for_start_at_boot(container)
+        add_tinc_interfaces(container)
+
+        @containers << container
+
+        #puts "Starting container #{container.name}"
+        #ShellCommand.exec("sudo vserver #{container.name} start", session)
+        #container.is_running = true
+      end
+
+      def add_tinc_interfaces(container)
+        tinc_ifaces = container.interfaces.select { |n| n.is_tinc_interface }
+        return if tinc_ifaces.count == 0
+        tinc_ifaces.each do |iface|
+          path = "#{container.config_path}/interfaces/#{iface.device_id}"
+          cmd = "
+          mkdir #{path};
+          echo #{iface.address} > #{path}/ip;
+          echo #{iface.device} > #{path}/dev;
+          echo #{iface.netmask} > #{path}/mask;
+          "
+          ShellCommand.exec("sudo sh -c \"#{cmd}\"", session)
+
+          ip = IPAddress("#{iface.address}/#{iface.netmask}")
+          path = "/etc/tinc/#{iface.device}/up.d/vserver-up"
+          cmd = "echo ip addr add #{iface.address}/#{ip.prefix} dev #{iface.device} > #{path}"
+          ShellCommand.exec("sudo sh -c \"#{cmd}\"", session)
+
+          path = "/etc/tinc/#{iface.device}/down.d/vserver-down"
+          cmd = "echo ip addr del #{iface.address}/#{ip.prefix} dev #{iface.device} > #{path}"
+          ShellCommand.exec("sudo sh -c \"#{cmd}\"", session)
+        end
+      end
+
+      def mark_for_start_at_boot(container)
+        path = "#{container.config_path}/apps/init/mark"
+        ShellCommand.exec("echo default | sudo tee #{path}", session)
+      end
+
+      def add_flags(container)
+        path = "#{container.config_path}/flags"
+        cmd = "echo \"VIRT_MEM\nVIRT_UPTIME\nVIRT_LOAD\nVIRT_CPU\" | sudo tee #{path}"
+        ShellCommand.exec(cmd, session)
+      end
+
+      def apply_memory_limits(container)
+        apply_memory_limits_cgroup(container)
+        apply_memory_limits_rlimits(container)
+      end
+
+      def apply_memory_limits_cgroup(container)
+        ram = container.ram
+        swap = container.ram + container.swap
+        cgroup_path = "#{container.config_path}/cgroup"
+        cmd = "
+        mkdir -p #{cgroup_path};
+        echo #{ram} > #{cgroup_path}/memory.limit_in_bytes;
+        echo #{swap} > #{cgroup_path}/memory.memsw.limit_in_bytes;
+        "
+        ShellCommand.exec("sudo sh -c \"#{cmd}\"", session)
+
+        if container.is_running && @cgroups_enabled
+          puts "Applying cgroup memory limits to running instance"
+          cgroup_path = "/dev/cgroup/#{container.name}"
+          cmd = "
+          echo #{ram} > #{cgroup_path}/memory.limit_in_bytes;
+          echo #{swap} > #{cgroup_path}/memory.memsw.limit_in_bytes;
+          "
+          ShellCommand.exec("sudo sh -c \"#{cmd}\"", session)
+        end
+      end
+
+      def apply_memory_limits_rlimits(container)
+        rlimit_path = "#{container.config_path}/rlimits"
+        ram = container.ram / 4
+        swap = container.ram + container.swap / 4
+        cmd = "
+        mkdir -p #{rlimit_path};
+        echo #{ram} > #{rlimit_path}/rss.soft;
+        echo #{swap} > #{rlimit_path}/rss.hard;
+        "
+        ShellCommand.exec("sudo sh -c \"#{cmd}\"", session)
+
+        if container.is_running && !@cgroups_enabled
+          puts "Applying rlimits memory limits to running instance"
+          cmd = "
+          vlimit -c #{container.ctx} -S --rss #{ram};
+          vlimit -c #{container.ctx} --rss #{swap};
+          "
+          ShellCommand.exec("sudo sh -c \"#{cmd}\"", session)
+        end
       end
 
       def next_context_id
         ids = Array(40000..40050)
         @containers.each do |c|
-          ids.delete_if { |id| c.context.to_s == id.to_s }
+          ids.delete_if { |id| c.ctx.to_s == id.to_s }
         end
         ids.first
       end
@@ -75,7 +171,7 @@ module Knife
         result = Array.new
         @containers.each do |c|
           c.interfaces.each do |iface|
-            result << iface.dev if dev =~ /dummy/
+            result << iface.device if iface.device =~ /dummy/
           end
         end
         result
@@ -90,6 +186,16 @@ module Knife
         raise "Container #{container.name} needs at least one ip address!" if container.interfaces.count == 0
         raise "Distribution #{container.distribution} isn't supported!" if container.distribution.match(/(squeeze|wheezy)/).nil?
         raise "Container #{container.name} has no hostname!" if container.hostname.nil?
+        container.interfaces.each do |iface|
+          raise "Device id for #{iface.address} is invalid!" if iface.device_id < 0
+        end
+
+        container.interfaces.select { |iface| iface.is_tinc_interface }.each do |iface|
+          unless ShellCommand.exec("sudo test -e /etc/tinc/#{iface.device}", @session,
+                               { :dont_raise_error => true }).succeeded?
+            raise "This host isn't configured for vpn #{iface.device}!"
+          end
+        end
       end
 
       def self.create(node, session)
@@ -118,6 +224,7 @@ module Knife
           interface_path = "#{c.config_path}/interfaces"
           ShellCommand.exec("sudo ls -ls #{interface_path} | egrep \"^. d\" | awk '{print $10}'", session).stdout.gsub(/\r/,"").strip.split("\n").each do |if_n|
             iface = Interface.new
+            iface.device_id = if_n
             iface.device = ShellCommand.exec("sudo cat #{interface_path}/#{if_n}/dev", session).stdout.strip
             iface.address = ShellCommand.exec("sudo cat #{interface_path}/#{if_n}/ip", session).stdout.strip
             if ShellCommand.exec("test -e #{interface_path}/#{if_n}/mask", session,
@@ -130,10 +237,12 @@ module Knife
           if host.cgroups_enabled &&
             ShellCommand.exec("sudo test -d #{c.config_path}/cgroup", session,
                               { :dont_raise_error => true }).succeeded?
+
             c.ram = ShellCommand.exec("sudo cat #{c.config_path}/cgroup/memory.limit_in_bytes", session).stdout.strip
             c.swap = ShellCommand.exec("sudo cat #{c.config_path}/cgroup/memory.memsw.limit_in_bytes", session).stdout.strip
           elsif ShellCommand.exec("sudo test -d #{c.config_path}/rlimits", session,
                                     { :dont_raise_error => true }).succeeded?
+
             c.ram = ShellCommand.exec("sudo cat #{c.config_path}/rlimits/rss.soft", session).stdout.strip.to_i * 4 * 1024
             c.swap = ShellCommand.exec("sudo cat #{c.config_path}/rlimits/rss.hard", session).stdout.strip.to_i * 4 * 1024
           end
--- lib/knife-vserver/interface.rb
+++ lib/knife-vserver/interface.rb
@@ -5,6 +5,13 @@ module Knife
       attr_accessor :device
       attr_accessor :address
       attr_accessor :netmask
+      attr_accessor :is_tinc_interface
+      attr_accessor :device_id
+
+      def initialize
+        @is_tinc_interface = false
+        @device_id = -1
+      end
 
       def to_s
         "#{@address}:#{@device}"
